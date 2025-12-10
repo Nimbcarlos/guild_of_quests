50202797b7128158de7564afd6be6cc97ad18a34
Nimbcarlos <consultor.nimb@hotmail.com>
2025-12-10 20:30:21 -0300

    dialogues goblins

--- teste.py
+++ teste.py
@@ -1,15 +1,15 @@
-# import json
+import json
 
-# with open("data/quests.json", "r", encoding="utf-8") as f:
-#     data = json.load(f)
-#     for q in data:
-#         print("id:", q['id'])
-#         print(q['name']['pt'], q['description']['pt'])
-#         print("Tipo:", q['type'], "| dificuldade:", q['difficulty'])
+with open("data/pt/dialogues.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    for q in data:
+        print("id:", q)
+        # print(q['name']['pt'], q['description']['pt'])
+        # print("Tipo:", q['type'], "| dificuldade:", q['difficulty'])
 
-from core.quest import Quest
+# from core.quest import Quest
 
-q = Quest.load_quests(language='pt')
-for i in q:
-    print(i.id)
-print(len(q))
+# q = Quest.load_quests(language='pt')
+# for i in q:
+#     print(i.id)
+# print(len(q))
