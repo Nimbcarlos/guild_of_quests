efa0aaf3202dbeba4f573f930b255552e6c50295
Nimbcarlos <consultor.nimb@hotmail.com>
2025-12-14 21:23:08 -0300

    dialogues quest mana

--- teste.py
+++ teste.py
@@ -1,9 +1,18 @@
 import json
 
-with open("data/pt/dialogues.json", "r", encoding="utf-8") as f:
+with open("data/quests.json", "r", encoding="utf-8") as f:
     data = json.load(f)
     for q in data:
-        print("id:", q)
+        print(q['id'])
+
+        # print("id:", q['id'],"\n",
+        #       "Nome:", q['name']['pt'],"\n",
+        #       q['description']['pt'],"\n",
+        #       "Apartir do turno:", q['available_from_turn'],"\n",
+        #       "Duração:", q['duration'],"\n",
+        #       "Depende da quest:", q['required_quests'],"\n",
+        #       "Nao pode ter completado a quesr id:", q['forbidden_quests'],"\n",
+        #       "hero necessario/proibido para aparecer", q['required_heroes'], q['forbidden_heroes'])
         # print(q['name']['pt'], q['description']['pt'])
         # print("Tipo:", q['type'], "| dificuldade:", q['difficulty'])
 
